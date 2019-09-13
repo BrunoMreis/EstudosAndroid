a9bbc373c051598c73830d96890d673546b78d57
BrunoMreis <brbrno@hotmail.com>
2019-09-13 10:38:11 -0300

    Layout FrameLayout

---
+++ FrameLayout/app/src/main/java/com/reis/framelayout/MainActivity.java
@@ -0,0 +1,26 @@
+package com.reis.framelayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
+
+public class MainActivity extends AppCompatActivity {
+    private FrameLayout frameLayout;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        frameLayout = findViewById(R.id.frameCarregamento);
+        frameLayout.setVisibility(View.GONE);
+    }
+
+    public void abrir(View view){
+        frameLayout.setVisibility(View.VISIBLE);
+    }
+}
